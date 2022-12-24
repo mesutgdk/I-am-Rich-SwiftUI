cfea7f842f613e0369c9ffa28743f21ba5296450
mesut <112901255+mesutgdk@users.noreply.github.com>
2022-12-23 23:30:44 -0800

    Add files via upload

---
+++ 16- I am rich/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  16- I am rich
+//
+//  Created by mesut on 11.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
